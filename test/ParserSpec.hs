5b7113c79714e09726c621c6612899425f577185
Cornelius Weig <cornelius.weig@tngtech.com>
2018-11-16 14:54:07 +0100

    Add test for empty lines in the header

--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -9,24 +9,25 @@ import           Text.Parsec.String    (Parser)
 import qualified Parser                as PA
 
 parseWithLeftOver :: Parser a -> String -> Either P.ParseError (a, String)
-parseWithLeftOver p = P.parse ((,) <$> p <*> leftOver) "timesheet parsing test"   
-    where 
-        leftOver = P.manyTill P.anyToken P.eof  
+parseWithLeftOver p = P.parse ((,) <$> p <*> leftOver) "timesheet parsing test"
+    where
+        leftOver = P.manyTill P.anyToken P.eof
 
 parseWithEof :: Parser a -> String -> Either P.ParseError a
 parseWithEof p = P.parse (p <* P.eof) "graph parsing test"
-
-comment :: String
-comment = "c whatevs \nc more whatevs \n"
 
 fileInfo :: String
 fileInfo = "p edge 5 5\n"
 
 spec :: Spec
 spec = do
-    describe "comments" $
+    describe "comments" $ do
        it "comment gets skipped" $
-            parseWithEof PA.header comment `shouldBe` Right ()
+            parseWithEof PA.header "c whatevs \nc more whatevs \n" `shouldBe` Right ()
+
+       it "empty lines in the comment are forbidden skipped" $
+            parseWithEof PA.header "c whatevs \n\nc more whatevs\n" `shouldSatisfy` isLeft
+
     describe "number of nodes" $
         it "I get the number of nodes" $
             parseWithEof PA.fileInfo fileInfo `shouldBe` Right 5
