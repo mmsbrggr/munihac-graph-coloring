a4980983de87cc42ff0af526995c5727f971356f
Marcel Moosbrugger <marcelmoosbrugger@gmail.com>
2018-11-16 15:17:07 +0100

    Added tests for Problem.hs

---
+++ test/ProblemSpec.hs
@@ -0,0 +1,32 @@
+module ProblemSpec where
+
+import           Test.Hspec (Spec, describe, it, shouldBe, shouldSatisfy)
+import qualified Data.Matrix as M
+import qualified Data.Vector as V
+import           Problem
+
+toyGraph :: Graph
+toyGraph = M.fromList 5 5 [ 0, 1, 0, 0, 1
+                        , 1, 0, 1, 1, 0
+                        , 0, 1, 0, 0, 0
+                        , 0, 1, 0, 0, 1
+                        , 1, 0, 0, 1, 0]
+
+coloring1 :: Coloring
+coloring1 = V.fromList [1, 2, 2, 3, 2]
+
+coloring2 :: Coloring
+coloring2 = V.fromList [1, 2, 3, 3, 2]
+
+spec :: Spec
+spec = do
+    describe "numberOfColors" $
+       it "should return the number of colors" $
+           numberOfColors coloring1 `shouldBe` 3
+
+    describe "numberOfConflicts" $ do
+        it "should return the right amount of conflicts" $
+            numberOfConflicts toyGraph coloring1 `shouldBe` 2
+    
+        it "should return no conflicts" $
+            numberOfConflicts toyGraph coloring2 `shouldBe` 0 
