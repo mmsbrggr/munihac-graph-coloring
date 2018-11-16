5ead54a960d9935080197e85a28ca5760aadb7ab
Marcel Moosbrugger <marcelmoosbrugger@gmail.com>
2018-11-16 14:46:07 +0100

    prettyfied Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -7,28 +7,32 @@ headlineFile = "headline.txt"
 filesFolder  = "files"
 
 main :: IO ()
-main = do greeting
-          filename <- askForFileName
-          graph <- parseFile filename
-          putStrLn (prettyMatrix graph)
-          pure ()
+main = do 
+    greeting
+    filename <- askForFileName
+    graph <- parseFile filename
+    putStrLn (prettyMatrix graph)
+    pure ()
 
 greeting :: IO ()
-greeting = do headline <- readFile headlineFile
-              putStrLn headline
-              putStrLn "Heuristic Graph Coloring"
-              putStrLn "by Cornelius, Giovanni, Marcel - Munihac (2018)"
-              putStrLn ""
+greeting = do 
+    headline <- readFile headlineFile
+    putStrLn headline
+    putStrLn "Heuristic Graph Coloring"
+    putStrLn "by Cornelius, Giovanni, Marcel - Munihac (2018)"
+    putStrLn ""
 
 askForFileName :: IO String
-askForFileName = do putStrLn "Name of DIMACS graph file:"
-                    filename <- getLine
-                    putStrLn ""
-                    pure filename
+askForFileName = do 
+    putStrLn "Name of DIMACS graph file:"
+    filename <- getLine
+    putStrLn ""
+    pure filename
 
 parseFile :: String -> IO (Matrix Int)
-parseFile filename = do putStrLn "Parsing file ..."
-                        matrix <- parseVertexFile (filesFolder ++ "/" ++ filename)
-                        putStrLn "... file parsed!"
-                        pure matrix
+parseFile filename = do 
+    putStrLn "Parsing file ..."
+    matrix <- parseVertexFile (filesFolder ++ "/" ++ filename)
+    putStrLn "... file parsed!"
+    pure matrix
 
