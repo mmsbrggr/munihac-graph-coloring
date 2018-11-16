cd889cf466129e1ba22866237f474ba51c9000fe
sphaso <sphaso@gmail.com>
2018-11-16 14:52:38 +0100

    cleanup

--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -1,19 +1,19 @@
 module ParserSpec where
 
-import           Control.Monad         (forM_) 
-import           Data.Either           (isLeft) 
-import           Data.Text             (pack) 
-import           Test.Hspec            (Spec, describe, it, shouldBe,                                         shouldSatisfy) 
-import qualified Text.Parsec           as P 
-import           Text.Parsec.String    (Parser) 
+import           Control.Monad         (forM_)
+import           Data.Either           (isLeft)
+import           Data.Text             (pack)
+import           Test.Hspec            (Spec, describe, it, shouldBe, shouldSatisfy)
+import qualified Text.Parsec           as P
+import           Text.Parsec.String    (Parser)
 import qualified Parser                as PA
 
-parseWithLeftOver :: Parser a -> String -> Either P.ParseError (a, String) 
+parseWithLeftOver :: Parser a -> String -> Either P.ParseError (a, String)
 parseWithLeftOver p = P.parse ((,) <$> p <*> leftOver) "timesheet parsing test"   
     where 
         leftOver = P.manyTill P.anyToken P.eof  
 
-parseWithEof :: Parser a -> String -> Either P.ParseError a 
+parseWithEof :: Parser a -> String -> Either P.ParseError a
 parseWithEof p = P.parse (p <* P.eof) "graph parsing test"
 
 comment :: String
