fb567d0c58e44ba29b36ae815835244f12fdf69e
Marcel Moosbrugger <marcelmoosbrugger@gmail.com>
2018-11-16 12:44:38 +0100

    Added comment for conflicts function

--- src/Problem.hs
+++ src/Problem.hs
@@ -9,6 +9,11 @@ type Coloring = V.Vector Int
 numberOfColors :: Coloring -> Int
 numberOfColors = V.maximum
 
+-- | Computes the number of conflicts for a graph and a given coloring:
+-- | It multiplies the adjecency matrix with the coloring diagonal-matrix.
+-- | Then in row i you have all the adjecent vertices in their respective color.
+-- | When subtracting the color vector from each column, the number of conflicts in the
+-- | original graph equals the number of zeros. That's what is computed by the function.
 numberOfConflicts :: Graph -> Coloring -> Int
 numberOfConflicts g c = foldr (\x c -> if x == 0 then c + 1 else c) 0 conflictmatrix
     where colmatrix      = multStd g (diagonal 0 c)
