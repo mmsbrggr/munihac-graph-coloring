93f81ac912c7303f07dcd483dc9f52eaff928882
j-mesnil <jonathan.mesnil@rwth-aachen.de>
2018-11-17 10:09:00 +0100

    performance enhancement for numberOfColors

--- src/Problem.hs
+++ src/Problem.hs
@@ -1,7 +1,8 @@
 module Problem where
 
-import Data.Matrix
-import Data.List
+import           Data.List
+import qualified Data.Map    as M
+import           Data.Matrix
 import qualified Data.Vector as V
 
 type Graph    = Matrix Int
@@ -14,7 +15,9 @@ maxDegree g = maximum $ multStd g vector
         vector = fromList nodes 1 (repeat 1)
 
 numberOfColors :: Coloring -> Int
-numberOfColors = length . nub . V.toList
+numberOfColors coloring = length . filter (/= 0). map snd . M.toList $ colorMap
+  where zeroMap = M.fromList $ zip [1.. V.length coloring] (repeat 0)
+        colorMap = foldr (M.adjust (+1)) zeroMap (V.toList coloring)
 
 -- | Computes the number of conflicts for a graph and a given coloring:
 -- | It multiplies the adjecency matrix with the coloring diagonal-matrix.
@@ -25,4 +28,4 @@ numberOfConflicts :: Graph -> Coloring -> Int
 numberOfConflicts g c = foldr (\x c -> if x == 0 then c + 1 else c) 0 conflictmatrix
     where colmatrix      = multStd g (diagonal 0 c)
           conflictmatrix = foldr modifyRow colmatrix [1..(nrows colmatrix)]
-          modifyRow r m  = mapRow (\_ x -> x - (c V.! (r - 1))) r m 
+          modifyRow r m  = mapRow (\_ x -> x - (c V.! (r - 1))) r m
