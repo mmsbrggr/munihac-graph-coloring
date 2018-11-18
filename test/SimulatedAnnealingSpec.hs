d26eb9f70238f6ffbd0dca34d4efb2484631f47f
lucazulian <info@lucazulian.it>
2018-11-18 09:25:41 +0100

    FIx tests

--- test/SimulatedAnnealingSpec.hs
+++ test/SimulatedAnnealingSpec.hs
@@ -17,7 +17,7 @@ toyGraph = M.fromList 5 5 [ 0, 1, 0, 0, 1
                           , 0, 1, 0, 0, 1
                           , 1, 0, 0, 1, 0]
 toyColoring :: Coloring
-toyColoring = V.fromList [1, 2, 3, 4, 5]
+toyColoring = V.fromList [1, 2, 2, 3, 2] 
 
 oldToyColoring :: Coloring
 oldToyColoring = V.fromList [5, 4, 3, 2, 1]
@@ -52,7 +52,7 @@ spec = do
             res1 `shouldNotBe` res2
 
         it "checks at least one color changes" $ do
-            res <- neighbor toyGraph 2 toyColoring $ toyRandom 2
+            res <- neighbor toyGraph 2 toyColoring $ toyRandom 1
             let zipped = V.zipWith (\x y -> if x /= y then 1 else 0) res toyColoring
             V.sum zipped `shouldSatisfy` \x -> x <= 1
 
