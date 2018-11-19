83dd8155c83575c83d08a7ad900d2d5667ed50f6
j-mesnil <jonathan.mesnil@rwth-aachen.de>
2018-11-19 22:03:12 +0100

    Visualisation: Fixed offbyones, edge representation working

--- src/Visualisation.hs
+++ src/Visualisation.hs
@@ -1,6 +1,9 @@
 module Visualisation where
 
+import qualified Debug.Trace    as D
+
 import           Control.Monad
+import           Data.Foldable
 import           Data.Matrix
 import qualified Data.Vector    as V
 import           Graphics.Gloss
@@ -12,8 +15,8 @@ import           Utils
 type Positioning = V.Vector (Int, Int)
 
 width, height, offset :: Int
-width = 520
-height = 520
+width = 500
+height = 500
 offset = 100
 
 window :: Display
@@ -23,35 +26,47 @@ background :: Color
 background = white
 
 dot :: Picture
-dot = circleSolid 5
+dot = circleSolid 10
 
 -- TODO: Build a more sophisticated version of this
 colorNode :: Int -> Int -> Color
 colorNode gColor cNum = makeColor
-  (fromIntegral gColor / fromIntegral cNum) 0 0 1
+  ((fromIntegral gColor) / (fromIntegral cNum)) 0 0 1
 
 -- TODO: Make this monster more readable
--- TODO: Build edges
 coloringToPic :: Graph -> Coloring -> Positioning -> Picture
-coloringToPic graph c pos = pictures $ V.toList $ fmap (\((px, py), gColor)
-  -> moveNode px py . color (colorNode gColor cNum) $ dot)  (V.zip pos c)
+coloringToPic graph c pos = pictures $ edgePic : nodePic
   where gSize = length pos
         cNum = numberOfColors c
-        moveNode x y = translate (fromIntegral x) (fromIntegral y)
+        moveNode x y = translate (toRange x) (toRange y)
+        toRange x = fromIntegral $ mod x height
+
+        nodePic :: [Picture]
+        nodePic = V.toList $ fmap (\((px, py), gColor)
+          -> moveNode px py . color (colorNode gColor cNum) $ dot)  (D.trace (show $ V.zip (fmap (\(x,y)->(toRange x, toRange y)) pos) c) (V.zip pos c))
+
+        mRows :: V.Vector (V.Vector Int)
+        mRows = fmap ((flip getRow) graph) $ V.fromList [1..gSize]
+
+        adjIndices :: V.Vector Int -> Int -> [Int]
+        adjIndices vec nNum = snd $ foldl' (\(n, xs) adj
+          -> (n+1, if adj == 1 then n:xs else xs))
+          (nNum + 1, []) (drop (nNum + 1) . V.toList $ vec)
+
+        edgesForNode :: Int -> [Int] -> [[(Int, Int)]]
+        edgesForNode node nodes = map (\x -> map (pos V.!) [node, x]) nodes
+
+        edges :: [[(Int, Int)]]
+        edges = concatMap (uncurry edgesForNode) . zip [0..]
+          . map (uncurry adjIndices) $ zip (V.toList mRows) [0..]
+
+        edgePic :: Picture
+        edgePic = pictures . map line $ D.trace (show $ flxy edges) (flxy edges)
+          where flxy = map $ map (\(x, y )-> (toRange x, toRange y))
 
 positionNodes :: Int -> IO Positioning
 positionNodes numNodes = liftM2 V.zip
   (V.replicateM numNodes (randomIO :: IO Int)) (V.replicateM numNodes (randomIO :: IO Int))
 
--- some Example
-drawing :: Picture
-drawing = pictures
-  [ translate (-20) (-100) $ color ballColor $ circleSolid 30
-  , translate 30 50 $ color paddleColor $ rectangleSolid 10 50
-  ]
-  where
-    ballColor = dark red
-    paddleColor = light (light blue)
-
-visualize :: IO ()
-visualize = display window background drawing
+visualize :: Picture -> IO ()
+visualize drawing = display window background drawing
